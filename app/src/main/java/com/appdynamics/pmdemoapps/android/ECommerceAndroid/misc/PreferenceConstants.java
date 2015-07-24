7971fc731ef89b4756aa4820b5d1e72cf6bab6a8
Mark Prichard <mqprichard@gmail.com>
2015-07-23 23:00:50 -0700

    Added default PreferenceConstants.java

---
+++ app/src/main/java/com/appdynamics/pmdemoapps/android/ECommerceAndroid/misc/PreferenceConstants.java
@@ -0,0 +1,7 @@
+package com.appdynamics.pmdemoapps.android.ECommerceAndroid.misc;
+
+public final class PreferenceConstants {
+    public final static String END_POINT_URL = ""; // Application URL
+    public static final String EUM_APP_KEY = ""; // EUM App Key
+    public static final String EUM_COLLECTOR_URL = ""; // EUM Collector URL
+}
